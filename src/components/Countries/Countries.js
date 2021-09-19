090f1069904c97d63b57d0e74a9fbea9dee0ca7b
Hasan75 <hasanjab14@gmail.com>
2021-09-20 01:21:57 +0600

    api set

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react/cjs/react.development';
 
 const Countries = () => {
+    const [countries, setCountries] = useState([])
+    useEffect(()=> {
+        fetch('https://restcountries.eu/rest/v2/all')
+        .then(res => res.json())
+        .then(data => setCountries(data))
+    },[])
     return (
         <div>
             <h1>Hello From Countries</h1>
