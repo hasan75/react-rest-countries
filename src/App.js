b6bb343078179649c754f314a5a06a0ec5ea6fb3
Hasan75 <hasanjab14@gmail.com>
2021-09-20 01:19:29 +0600

    with different folder initialization

--- src/App.js
+++ src/App.js
@@ -1,47 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
-import { useEffect } from 'react/cjs/react.development';
+import Countries from './components/Countries/Countries';
+
 
 function App() {
   return (
     <div className="App">
-      <Countries></Countries>
-      
+    <Countries></Countries>
     </div>
   );
 }
 
-function Countries (){
-  const [countries, setCountries] = useState([])
-  useEffect(()=>{
-    fetch('https://restcountries.eu/rest/v2/all')
-    .then(res => res.json())
-    .then( data => setCountries(data))
-  },[])
 
-  return(
-    <div>
-      <h2>Travelling Around The World</h2>
-      <h4>Countries Available: {countries.length}</h4>
-      <div className="countries">
-      {
-        countries.map(country => <Country name={country.name} flag={country.flag} capital={country.capital}></Country>)
-      }
-      </div>
-    </div>
-  )
-}
 
-function Country(props){
-  console.log(props)
-  return(
-    <div className="country">
-      <h3>Country Name: {props.name}</h3>
-      <h4>Capital: {props.capital}</h4>
-      <img src={props.flag}alt="country_picture"></img>
-    </div>
-  )
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Countries (){
+//   const [countries, setCountries] = useState([])
+//   useEffect(()=>{
+//     fetch('https://restcountries.eu/rest/v2/all')
+//     .then(res => res.json())
+//     .then( data => setCountries(data))
+//   },[])
+
+//   return(
+//     <div>
+//       <h2>Travelling Around The World</h2>
+//       <h4>Countries Available: {countries.length}</h4>
+//       <div className="countries">
+//       {
+//         countries.map(country => <Country name={country.name} flag={country.flag} capital={country.capital}></Country>)
+//       }
+//       </div>
+//     </div>
+//   )
+// }
+
+// function Country(props){
+//   console.log(props)
+//   return(
+//     <div className="country">
+//       <h3>Country Name: {props.name}</h3>
+//       <h4>Capital: {props.capital}</h4>
+//       <img src={props.flag}alt="country_picture"></img>
+//     </div>
+//   )
+// }
 
 export default App;
