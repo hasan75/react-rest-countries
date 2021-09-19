b5966e962769c86e5e1420452465fa07036bf6c6
Hasan75 <hasanjab14@gmail.com>
2021-09-19 23:55:27 +0600

    countries showed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react/cjs/react.development';
 
 function App() {
   return (
@@ -11,10 +13,32 @@ function App() {
 }
 
 function Countries (){
+  const [countries, setCountries] = useState([])
+  useEffect(()=>{
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then( data => setCountries(data))
+  },[])
 
   return(
     <div>
       <h2>Travelling Around The World</h2>
+      <h4>Countries Available: {countries.length}</h4>
+      <div className="countries">
+      {
+        countries.map(country => <Country name={country.name} flag={country.flag}></Country>)
+      }
+      </div>
+    </div>
+  )
+}
+
+function Country(props){
+  console.log(props)
+  return(
+    <div className="country">
+      <h3>Country Name: {props.name}</h3>
+      <img src={props.flag}alt="country_picture"></img>
     </div>
   )
 }
