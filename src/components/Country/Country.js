715ef40f691698bacf8d1a7b1481ba7d8f7735b9
Hasan75 <hasanjab14@gmail.com>
2021-09-20 01:54:28 +0600

    style added

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Country.css'
 
 const Country = (props) => {
     return (
-        <div>
+        <div className="country">
             <h4>Name: {props.name}</h4>
             <h5>Capital: {props.capital}</h5>
             <h5>Population: {props.population}</h5>
